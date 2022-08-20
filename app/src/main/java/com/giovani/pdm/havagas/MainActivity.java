ee872eddc53c1eeb541b12a5ee7bbd633f62c14f
GiovaniCappelaro <giovani.c@aluno.ifsp.edu.br>
2022-08-20 14:24:57 -0300

    Lógica botão limpar form MainActivity

--- app/src/main/java/com/giovani/pdm/havagas/MainActivity.java
+++ app/src/main/java/com/giovani/pdm/havagas/MainActivity.java
@@ -8,18 +8,46 @@ import com.giovani.pdm.havagas.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Objetos da classe ActivityMainBinding e Candidato
     private ActivityMainBinding amb;
-
+    private Candidato candidato;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //instanciar o objeto da classe ActivityMainBinding
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(amb.getRoot()); //view vai ser a definida como root da Activity
 
 
+        /*---------LISTENNERS---------*/
 
 
-        setContentView(R.layout.activity_main);
+        //Limpar o form
+        amb.limparBt.setOnClickListener(view -> {
+            amb.nomeCompletoEt.setText("");
+            amb.emailEt.setText("");
+            amb.desejoAtualizacoesCb.setChecked(false);
+            amb.telefoneEt.setText(null);   //ver se null serve pra numero mesmo
+            amb.isTelefoneComercialCb.setChecked(false);
+            amb.addCelularCb.setChecked(false);
+            amb.masculinoRb.setChecked(true);
+            amb.dataDeNascimentoEt.setText("");
+            amb.formacaoSp.setSelection(0);
+            amb.vagasInteresse.setText("");
+            candidato = null;   //zerar o objeto
+        });
+
     }
+
+
+
+
+
+
+
+
 }
